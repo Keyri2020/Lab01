cb497bb3917f3e7f7cf00dc6dbeb18c78d1c2495
Cristina20211 <87824517+Cristina20211@users.noreply.github.com>
2022-03-07 10:24:36 -0600

    Conexion_EntidadesContacto

--- Laboratorio/src/main/java/Entidades/Contacto.java
+++ Laboratorio/src/main/java/Entidades/Contacto.java
@@ -10,4 +10,69 @@ package Entidades;
  */
 public class Contacto {
     
+    
+    
+     String Nombre;
+     String Apellido;
+      int Edad;
+     String Email;
+     String NumerodeTelefono;
+
+   
+   
+   
+    public String getNombre() {
+        return Nombre;
+    }
+   
+     public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+    
+     public String getApellido() {
+        return Apellido;
+    }
+   
+   
+    public void setApellido(String Apellido) {
+        this.Apellido = Apellido;
+    }
+
+      public int getEdad() {
+        return Edad;
+    }
+
+      public void setEdad(int Edad) {
+        this.Edad = Edad;
+    }
+
+       public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    
+       public String getNumerodeTelefono() {
+        return NumerodeTelefono;
+    }
+   
+  
+    public void setNumerodeTelefono(String NumerodeTelefono) {
+        this.NumerodeTelefono = NumerodeTelefono;
+    }
+
+  
+    
+  
+
+ 
+
+  
+ 
+ 
+   
+   
 }
--- Laboratorio/src/main/java/com/cris/BD/ConexionAMYSQL.java
+++ Laboratorio/src/main/java/com/cris/BD/ConexionAMYSQL.java
@@ -4,10 +4,43 @@
  */
 package com.cris.BD;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author MINEDUCYT
  */
 public class ConexionAMYSQL {
+    
+     
+    private static Connection ConnectionBD = null;
+    public Connection getConnection (){
+    
+  
+    
+    
+        try {
+            String url ="jdbc:mysql://localhost:3306/laboratorio";
+            String user="root";
+            String password="root";
+            
+            ConnectionBD=DriverManager.getConnection(url,user,password);
+            System.out.println("conexion exitosa");
+            
+            
+        } catch (Exception e) {
+        
+            JOptionPane.showMessageDialog(null,"Error"+e.toString());
+        
+        }
+    
+    
+       return ConnectionBD;
+    }
+    
+    
+    
     
 }
